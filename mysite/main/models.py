3c88e17bc59979fefd99ac3aea22114bc5f781d5
usualcarl <mlb.chingiz@gmail.com>
2024-11-28 18:15:28 +0500

    Initial commit

---
+++ mysite/main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return  self.name
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    durations =  models.DurationField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
---
+++ mysite/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+# Create your views here.
